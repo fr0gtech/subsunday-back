9decda582589042811b7d0e53a05b597277efe7f
GREEB <gr44bli@gmail.com>
2025-05-10 18:12:33 +0200

    fix: if we cant find steam on update dont do anything

--- app/lib.ts
+++ app/lib.ts
@@ -98,6 +98,8 @@ export const updateGame = async (gameOnDb: Game | null) => {
       return;
     }
   }
+
+  if (!steamId) return
   console.log("update game", steamId);
     
   // we got a game on db but need to update it with steam info if we got any new ones
